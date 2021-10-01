1d7e7a5e9f909be7bdcdb879561cd0ab5c5362bf
Dzmitry Zhybryk <jibrikdima@gmail.com>
2021-10-01 20:37:57 +0300

    add new class HTTPClient

---
+++ parsing_onliner/http_client.py
@@ -0,0 +1,23 @@
+"""Storage module for class HTTPClient"""
+import requests
+from requests import Response
+
+
+class HTTPClient:
+    """Class gets http from link"""
+
+    @staticmethod
+    def get(url: str, headers=None, params=None) -> Response:
+        return requests.get(url, headers=headers, params=params)
+
+    @staticmethod
+    def post(url: str, headers=None, params=None, json=None, data=None) -> Response:
+        return requests.post(url, headers=headers, params=params, json=json, data=data)
+
+    @staticmethod
+    def put(url: str, headers=None, params=None, json=None, data=None) -> Response:
+        return requests.put(url, headers=headers, params=params, json=json, data=data)
+
+    @staticmethod
+    def delete(url: str, headers=None, params=None) -> Response:
+        return requests.delete(url, headers=headers, params=params)
