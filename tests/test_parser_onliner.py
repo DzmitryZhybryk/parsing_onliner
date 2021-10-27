edce6be8075f9d24909b5a4970547e21bcc13cd7
Dzmitry Zhybryk <jibrikdima@gmail.com>
2021-10-27 17:49:36 +0300

    add error message change some tests name

--- tests/test_parser_onliner.py
+++ tests/test_parser_onliner.py
@@ -2,21 +2,20 @@ import pytest
 
 
 def test_onliner_categories_count(test_data, actual_categories_number):
-    assert actual_categories_number == test_data.get('excepted_result'), 'Error message'
+    assert actual_categories_number == test_data.get('excepted_result'), 'The number of categories should be six!'
 
 
 @pytest.mark.parametrize('test_input', ['авто', 'машина'])
-def test_onliner_auto_categories_links(test_input, chek_word_in_text):
+def test_onliner_auto_categories_articles(test_input, chek_word_in_text):
     for items in chek_word_in_text:
-        assert test_input in items, 'Error message'
+        assert test_input in items, f'All auto categories articles should to have {test_input} word!'
 
 
-@pytest.mark.xfail(strict=True)
 @pytest.mark.parametrize('test_input', ['Минск'])
-def test_onliner_people_categories_links(test_input, chek_word_in_text):
+def test_onliner_people_categories_articles(test_input, chek_word_in_text):
     for items in chek_word_in_text:
-        assert test_input not in items, 'Error message'
+        assert test_input not in items, f'All people categories should not have {test_input} word!'
 
 
 def test_onliner_article_is_not_empty(test_data, article_is_not_empty):
-    assert article_is_not_empty > 0, 'Error message'
+    assert article_is_not_empty > 0, 'The article should not be empty!'
